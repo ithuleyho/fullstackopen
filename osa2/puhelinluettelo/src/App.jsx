a38511ba7c8edf330a3b3543ff78174650d98814
Ithuleyho <ithuleyho@proton.me>
2024-09-05 09:19:21 +0300

    Osa 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,62 @@
 import { useState } from "react";
 
+const Filter = ({ setFilter }) => {
+  return (
+    <div>filter shown with <Input setter={setFilter}/></div>
+  );
+};
+
+const Input = ({ value, setter }) => {
+  const handler = (event) => {
+    setter(event.target.value);
+  };
+
+  return (
+    <input value={value} onChange={handler} />
+  );
+};
+
+const PersonForm = ({ newEntry }) => {
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    };
+    if (newEntry(newPerson).succeeded) {
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+  
+  return (
+    <form>
+      <h2>Add new entry</h2>
+      <div>name: <Input value={newName} setter={setNewName} /></div>
+      <div>number: <Input value={newNumber} setter={setNewNumber} /></div>
+      <div>
+        <button type="submit" onClick={addPerson}>add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ filter, persons }) => {
+  const filtered = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+
+  return (
+    <>
+      <h2>Numbers</h2>
+      {filtered.map(person => (
+        <p key={person.name}>{person.name} {person.number}</p>
+      ))}
+    </>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -7,52 +64,23 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
 
-  const handleNameInput = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberInput = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterInput = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const newEntry = (event) => {
-    event.preventDefault();
-    const newPerson = {
-      name: newName,
-      number: newNumber
-    };
+  const newEntry = (newPerson) => {
     if (persons.find(person => person.name === newPerson.name)) {
       alert(`${newPerson.name} is already added to phonebook`);
-      return;
+      return {succeeded: false};
     }
-    setPersons([newPerson, ...persons]);
+    setPersons(persons.concat(newPerson));  
+    return {succeeded: true};
   };
-
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input onChange={handleFilterInput}/></div>
-      <h2>Add new entry</h2>
-      <form>
-        <div>name: <input onChange={handleNameInput} /></div>
-        <div>number: <input onChange={handleNumberInput} /></div>
-        <div>
-          <button type="submit" onClick={newEntry}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.filter(person => person.name.toLowerCase().includes(filter.toLocaleLowerCase()))
-        .map(person => (
-          <p key={person.name}>{person.name} {person.number}</p>
-      ))}
+      <Filter setFilter={setFilter} />
+      <PersonForm newEntry={newEntry} />
+      <Persons filter={filter} persons={persons} />
     </div>
   );
 };
