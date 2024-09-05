6fa5baef79357cd8e81d6668b66dfe41c0b46846
Ithuleyho <ithuleyho@proton.me>
2024-09-05 08:22:38 +0300

    Osa 2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,45 +1,4 @@
-const Header = ({course}) => {
-  return (
-    <h1>{course}</h1>
-  );
-};
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map(part => 
-        <Part part={part} />
-      )}
-    </>
-  );
-};
-
-const Total = ({parts}) => {
-  const sum = parts.reduce((acc, curr) => acc + curr.exercises, 0);
-  return (
-    <p>
-      Number of exercises {sum}
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );  
-};
+import Course from './Course';
 
 const App = () => {
   const courses = [
