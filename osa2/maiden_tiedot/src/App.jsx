fb229fe8d218754c9d474179478cb8dc10d87aa6
Ithuleyho <ithuleyho@proton.me>
2024-09-05 17:23:50 +0300

    Osa 2.20

--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 
 const CountryInformation = ({ countries, country, showCountryHandler }) => {
-  console.log(countries);
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   }
@@ -32,13 +31,27 @@ const CountryInformation = ({ countries, country, showCountryHandler }) => {
         </div>
       ))}
     </>
+  );
+};
+
+const Weather = ({ city }) => {
+  return (
+    <div>
+      <h1>Weather in { city.name }</h1>
+      <p>temperature {(city.main?.temp - 273.15).toFixed(2)} Celcius</p>
+      <img src={`https://openweathermap.org/img/wn/${city.weather[0]?.icon}@2x.png`} />
+      <p>wind {city.wind.speed} m/s</p>
+    </div>
   )
-}
+};
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState(null);
   const [filter, setFilter] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  const api_key = import.meta.env.VITE_WEATHERAPI_KEY;
   
   useEffect(() => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
@@ -60,6 +73,15 @@ const App = () => {
       axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
         .then(response => {
           setCountry(response.data);
+          const [lat, lon] = response.data.capitalInfo.latlng;
+          axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
+            .then(response => {
+              setWeather(response.data);
+            })
+            .catch(e => {
+              setWeather(null);
+              console.log(e);
+            });
         });
     } else {
       setCountry(null);
@@ -69,13 +91,13 @@ const App = () => {
   if (countries === null) {
     return null;
   }
-
   return (
     <div>
       <p>find countries <input value={filter} onChange={event => setFilter(event.target.value)}/></p>
       <CountryInformation countries={filteredCountries} country={country} showCountryHandler={name => {console.log(name)}}/>
+      {country && <Weather city={weather} />}
     </div>
-  )
+  );
 };
 
 export default App
